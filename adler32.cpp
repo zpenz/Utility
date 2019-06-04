73b4b611eb5afa33e6bebbd6e440718557509633
szp2076250 <2361489331@qq.com>
2019-06-04 23:16:14 +0800

    reserve

--- adler32.cpp
+++ adler32.cpp
@@ -184,73 +184,6 @@ struct range{
     range(){}
 };
 
-// AString diff(AString e,AString f,int i =0,int j=0){
-//     int N = e._length();
-//     int M = f._length();
-//     int L = N+M;
-//     int Z = 2*min(N,M)+2;
-
-//     if(N>0 && M >0){
-//         int w = N-M;
-//         vector<int> g = vector<int>(Z);
-//         vector<int> p = vector<int>(Z);
-//         for (int h=0;h<(L/2+L%2+1);h++)
-//         {
-//             for(int r =0;r<2;r++){
-//                 auto c = r==0?g:p;
-//                 auto d = r==0?p:g;
-//                 auto o = r==0?1:0;
-//                 auto m = r==0?1:-1;
-
-//                 for(int k = -(h-2*max(0,h-M));k<h-2*max(0,h-N)+1;k+=2){
-//                     auto a = (k==-h || k!=h && c[(k-1)%Z]<c[(k+1)%Z])?c[(k+1)%Z]:c[(k-1)%Z]+1;
-//                     auto b = a -k;
-//                     auto s = a;
-//                     auto t = b;
-//                     while(a<N && b<M && e.At((1-o)*N+m*a+(o-1)) == f.At((1-o)*M+m*b+(o-1))){
-//                          a++;
-//                          b++;   
-//                     } 
-//                     c[k%Z] = a;
-//                     auto z = -(k-w);
-//                     if(L%2 == o && z>=-(h-o) && z<=h-o && c[k%z]+d[z%Z] >=N){
-//                         auto D = o==1?2*h-1:2*h;
-//                         auto x = o==1?s:N-a;
-//                         auto y = o==1?t:M-b;
-//                         auto u = o==1?a:N-s;
-//                         auto v = o==1?b:M-t;
-//                         if(D>1 || (x!=u && y!=v)){
-//                             return diff(e.substr(0,x),f.substr(0,y),i,j)+diff(e.substr(u,N-u),f.substr(v,M-v),i+u,j+v); 
-//                         }else if(M>N){
-//                             return diff("",f.substr(N,M-N),i+N,j+N); 
-//                         }else if(M<N){
-//                             return diff(e.substr(M,N-M),"",i+M,j+M);
-//                         }else{
-//                            return "null"; 
-//                         }
-                        
-//                     }
-//                 }
-//             }
-//         }
-        
-//     }else if(N>0){
-//         AString a = "";
-//         for(int n=0;n<N;n++){
-//             a+="old_position: "+AString(n+i);
-//         }
-//         return "delete"+a;
-//     }
-//     else{
-//         AString b = "";
-//         for(int n=0;n<N;n++){
-//             b+="old_position: "+AString(i)+" new_position:";
-//             b+=j+n;
-//         }
-//         return "insert"+b;
-//     }
-// }
-
 enum class ActionType{
     ACTION_INSERT,
     ACTION_DELETE,
@@ -265,6 +198,7 @@ struct Diff{
 
 struct Prev{
     int x;
+    int pk;
     Prev(int _x):x(_x){}
 };
 
@@ -392,9 +326,8 @@ void Reverse(AString a,AString b){
     vector<decltype(list)> dlist;
     list[-1 + offset] = -1;
 
-    bool solve = false;
-
-    for(int d=0;d<=sizea+sizeb && !solve;d++){
+    int countd = 0;
+    for(int d=0;d<=sizea+sizeb;d++){
         printf("d=%d: \n",d);
         for(int k=-d;k<=d;k+=2){
 
@@ -423,75 +356,60 @@ void Reverse(AString a,AString b){
             SHOW_MESSAGE(yEnd, 1);
 
             list[k+offset] = yEnd;
-
-            //solution
-            if(xEnd>=sizea && yEnd>=sizeb){
-                solve = true;
+            list[k+offset].pk = kPrev;
+            
+            if (xEnd >= sizea && yEnd >= sizeb)
+            {
+                //solution
+                dlist.push_back(list);
+                for(int index=countd;index>0;index--){
+                    SHOW_MESSAGE(k,1);
+                    SHOW_MESSAGE(yEnd,1);
+                    k = ((dlist[index])[k+offset]).pk;
+                    yEnd = ((dlist[index])[k + offset]).x;
+                }
+                return;
             }
         }
+        countd++;
         dlist.push_back(list);
     }
 
+    
     //solve
-    vector<Snake> sl;
+    // vector<Snake> sl;
 
-    Point pt = Point(sizea,sizeb);
-    for(int d = dlist.size()-1;pt.x>0 || pt.y>0;d--){
-        int k = pt.y-pt.x;
-        auto v = dlist[d];
-        bool up = k==d ||(k!=-d && v[offset+k-1].x-(k-1) > v[offset+k+1].x-(k+1));
+    // Point pt = Point(sizea,sizeb);
+    // for(int d = dlist.size()-1;pt.x>0 || pt.y>0;d--){
+    //     int k = pt.y-pt.x;
+    //     auto v = dlist[d];
+    //     bool up = k==d ||(k!=-d && v[offset+k-1].x-(k-1) > v[offset+k+1].x-(k+1));
 
-        int yEnd = v[k+offset].x;
-        int xEnd = yEnd - k;
+    //     int yEnd = v[k+offset].x;
+    //     int xEnd = yEnd - k;
 
-        int kPrev   = up?k-1:k+1;
+    //     int kPrev   = up?k-1:k+1;
 
-        int yStart = v[kPrev + offset].x;
-        int xStart = yStart - kPrev;
+    //     int yStart = v[kPrev + offset].x;
+    //     int xStart = yStart - kPrev;
 
-        int yMiddle = up?yStart + 1:yStart; 
-        int xMiddle = yMiddle - k;
+    //     int yMiddle = up?yStart + 1:yStart; 
+    //     int xMiddle = yMiddle - k;
 
-        sl.push_back(Snake(Point(xStart,yStart),Point(xMiddle,yMiddle),Point(xEnd,yEnd)));
+    //     sl.push_back(Snake(Point(xStart,yStart),Point(xMiddle,yMiddle),Point(xEnd,yEnd)));
 
-        pt.x = xStart;
-        pt.y = yStart;
-    }
+    //     pt.x = xStart;
+    //     pt.y = yStart;
+    // }
 
-    for_each(sl.rbegin(), sl.rend(), [](Snake &item) {
-        SHOW_MESSAGE(item.start, 1);
-        SHOW_MESSAGE(item.mid, 1); 
-    }); 
+    // for_each(sl.rbegin(), sl.rend(), [](Snake &item) {
+    //     SHOW_MESSAGE(item.start, 1);
+    //     SHOW_MESSAGE(item.mid, 1); 
+    // }); 
 }
 
 int main(int argc, char const *argv[])
 {
-    // vector<FileData> fl;
-    // vector<ChunkData> diff;
-    // vector<range> rangelist;
-    // // auto ret1 = CalcFileDiff("1.txt");          //old
-
-    // FileData ret1 = CalcFileDiff("1.txt");
-    // FileData ret2 = CalcFileDiff("1.txt.diff");
-    // // auto ret2 = CalcFileDiff("1.txt.diff");     //new
-
-    // int indexX = 0,indexY = 0;
-    // range rg;
-    // bool  havediff =true;
-    // int start = 0;
-    // int length = 0;
-    
-    // SHOW_MESSAGE(diff("abcdef","acf"),1);
-
     Reverse("abcabba", "cbabac");
-
-    // for_each(rangelist.begin(),rangelist.end(),[&](range & item){
-    //     SHOW_MESSAGE(item.start,1);
-    //     SHOW_MESSAGE(item.length,1);
-    // });
-    
-    // SaveDiff<FileData>("sync.diff",fl);
-    // FileMarge("1.txt",2,"123",4096);
-    // auto ret = LoadDiff<FileData>("sync.diff");
     return 0;
 }
